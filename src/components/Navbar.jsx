332aa3251b07de08bacb02859afc26484a289555
pawanbondre67 <bondrepavan623@gmail.com>
2024-02-09 19:40:04 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { AuthContext } from './context/AuthContext';
 const Navbar = () => {
 
   const {currentUser} = useContext(AuthContext)
+  
   return (
    <div className="navbar">
     <span className="logo">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,103 @@
-import React from 'react'
-import user from '../assets/abhishek.jpeg';
+import React , {useState , useContext} from 'react'
+
+import { collection, query, serverTimestamp, updateDoc, where, getDocs, getDoc, setDoc } from "firebase/firestore";
+import { db } from '../firebase';
+import { AuthContext } from './context/AuthContext';
+
 
 const Search = () => {
+
+   const [username , setUsername] = useState("");
+   const {currentUser} = useContext(AuthContext);
+   const [user , setUser] = useState(null);
+
+   const [err , setErr] = useState(false)
+  
+    const handleSearch = async () => {
+
+      const q =  query(collection(db, "users"), where("displayName", "==", username));
+ 
+      try{
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          setUser(doc.data());
+        });
+  }catch(err){
+    setErr(true);
+  }
+
+    };
+    
+   const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+   };
+
+   const handleSelect =  async() => {
+    //check whether the group(chats in firestore) exists , if not create one
+    
+      const combinedId =   
+      currentUser.uid > user.uid 
+      ? currentUser.uid + user.uid 
+      : user.uid + currentUser.uid;
+
+      try {
+        const res = await getDoc(doc(db , "chats", combinedId));
+         
+if(!res.exists()){  // firebase method to check whether the document exists
+
+  //create a chat in chats collection
+  await setDoc(doc(db, "chats", combinedId), {
+    messages: []
+  });
+
+  //create user chats
+ await updateDoc(doc(db, "userChat", currentUser.uid), {
+  [combinedId+".userInfo"]: {
+    uid: user.uid,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+  },
+  [combinedId+".date"]: serverTimestamp()
+      
+ });
+
+ await updateDoc(doc(db, "userChat", user.uid), {
+  [combinedId+".userInfo"]: {
+    uid: currentUser.uid,
+    displayName: currentUser.displayName,
+    photoURL: currentUser.photoURL,
+  },
+  [combinedId+".date"]: serverTimestamp()
+      
+   });
+  }
+}     catch (err) {
+  
+        
+      }
+    //create user chats
+
+   }
+
+
   return (
     <div className="search">
       <div className="searchbox">
-        <input className='user-input'   type="text" placeholder='find a user'/>
+        <input className='user-input'   type="text" placeholder='find a user' onKeyDown={handleKey}  onChange={e=>setUsername(e.target.value)}/>
       </div>
+     {err && <span>User not found</span>}
 
-      <div className="userchat">
-        <img  className='userprofile'  src={user} alt="user-profile" />
+      {user && <div className="userchat" onClick={handleSelect}>
+        <img  className='userprofile'  src={user.photoURL} alt="user-profile" />
         <div className="userchatinfo">
-          <span>Abhishek</span>
-          <p>hello</p>
+          <span>{user.displayName}</span>
+          
         </div>
       </div>
+        }
     </div>
-  )
-}
+  );
+};
 
 export default Search
